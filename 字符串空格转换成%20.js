569f94bf6aef61cf597208a2559e727950543016
jackson <1060428669@qq.com>
2021-03-22 15:01:20 +0800

    event loop

--- 字符串空格转换成%20.js
+++ 字符串空格转换成%20.js
@@ -1,22 +1,22 @@
-// const replaceSpace = function (str) {
-//     const newStr = []
-//     // i 是新字符串的index
-//     // j 是原字符串的index
-//     for (let i = 0, j = 0, k = 0; j < str.length; j++) {
-//         if (str[j] === " ") {
-//             newStr[i++] = "%";
-//             newStr[i++] = "2";
-//             newStr[i++] = "0";
-//             console.log('i= ', i)
-//         } else {
-//             newStr[i++] = str[j];
-//         }
-//     }
-//     return newStr.join("");
-// };
-// console.log(replaceSpace('We Are Happy'))
+const replaceSpace = function (str) {
+    const newStr = []
+    // i 是新字符串的index
+    // j 是原字符串的index
+    for (let i = 0, j = 0, k = 0; j < str.length; j++) {
+        if (str[j] === " ") {
+            newStr[i++] = "%";
+            newStr[i++] = "2";
+            newStr[i++] = "0";
+            console.log('i= ', i)
+        } else {
+            newStr[i++] = str[j];
+        }
+    }
+    return newStr.join("");
+};
+console.log(replaceSpace('We Are Happy'))
 
-// const replaceSpace = function (str) {
-//     return str = str.split(' ').join("%20");
-// };
-// console.log(replaceSpace('We Are Happy'))
+const replaceSpace = function (str) {
+    return str = str.split(' ').join("%20");
+};
+console.log(replaceSpace('We Are Happy'))

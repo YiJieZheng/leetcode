9375a757f0774d85a4c3dd0c581f698c18310937
jackson <1060428669@qq.com>
2021-03-23 13:49:35 +0800

    算法更新

--- 0到n-1中缺失的数.js
+++ 0到n-1中缺失的数.js
@@ -2,7 +2,7 @@
 //1.判断头尾是否缺失
 //2.二分法查找如果中间值不缺失说明缺失的数在mid右边，相反在左边
 //3.判断第一个数是否缺失
-let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10]
+let arr = [0, 2, 3, 4, 5, 6, 7, 8, 9]
 function getNum(arr) {
     let l = 0;
     let r = arr.length - 1
@@ -15,5 +15,6 @@ function getNum(arr) {
         arr[mid] === mid ? l = mid + 1 : r = mid - 1
     }
     return l
+    //因为数列是递增的所以缺失的数字一定在mid的左边
 }
 console.log(getNum(arr))

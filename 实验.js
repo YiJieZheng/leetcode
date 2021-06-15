6cf4b3a5f2a9ea8460e72f435dc869ea6c422cf8
jackson <1060428669@qq.com>
2021-06-15 17:02:19 +0800

    测试代码

---
+++ 实验.js
@@ -0,0 +1,18 @@
+// let startTime = new Date().getTime();
+// let count = 0;
+// //耗时任务
+// // setInterval(function () {
+// //     let i = 0;
+// //     while (i++ < 1000000000);
+// // }, 0);
+// for (var i = 0; i < 100000; i++) {
+//     let a = i
+// }
+// setInterval(function () {
+//     count++;
+//     console.log("时间差：", (new Date().getTime() - (startTime + count * 1000)), "毫秒")
+// }, 1000);
+
+for (var item = 0; item < 10; item++) {
+    console.log(global)
+}
